e0a2ed7a60ad8953506e4ccf5a30ff42c35bc9b4
Wh0_ <58286139+Vendetta2003@users.noreply.github.com>
2021-11-14 03:31:42 +0530

    Update netscanner0.py
    Added time module to check time taken for the Scan.

--- netscanner0.py
+++ netscanner0.py
@@ -1,20 +1,23 @@
 import threading
 import os 
 import sys
-# usage  - python netscanner0.py <Number of hosts to scan in the current network>
+import time 
+# usage  - python netscanner.py <Number of hosts to scan in the current network>
 # it uses 192.168.0.1-255 as its ip range. 
 
 def main():
+    print()	
     def ping(ip):
         resp = str(os.popen(f"ping -n 1 {ip}").read())
         if ("Destination host unreachable" not in resp):
-            print(f"[+] Host is up - {ip}")
+            print(f"[+]Host is up - {ip}")
         else:
-            print(f"[-] Host is down - {ip}")
+            print(f"[-]Host is down - {ip}")
         
     threads  = []
     a = int(sys.argv[1])
     a = a+1
+    t1 = time.time()
     
     for x in range(a):
         t = threading.Thread(target=ping , args=(f"192.168.0.{x}",))
@@ -29,6 +32,9 @@ def main():
     except Exception as e :
         print(e)
         pass
-
+    
+    t2 = time.time()
+    print()
+    print(f"(.Y.) Scanned {a} Hosts  in {t2-t1} seconds.")
 if __name__ == "__main__":
     main()
