a5f9fc0086c1409a9d6db79087e6c85d5c26bd5f
Wh0_ <58286139+Vendetta2003@users.noreply.github.com>
2021-11-14 03:17:30 +0530

    Add files via upload
    A basic network scanner which I made to understand threading and learn some os/sys modules.

---
+++ netscanner0.py
@@ -0,0 +1,34 @@
+import threading
+import os 
+import sys
+# usage  - python netscanner.py <Number of hosts to scan in the current network>
+# it uses 192.168.0.1-255 as its ip range. 
+
+def main():
+    def ping(ip):
+        resp = str(os.popen(f"ping -n 1 {ip}").read())
+        if ("Destination host unreachable" not in resp):
+            print(f"[+]Host is up - {ip}")
+        else:
+            print(f"[-]Host is down - {ip}")
+        
+    threads  = []
+    a = int(sys.argv[1])
+    a = a+1
+    
+    for x in range(a):
+        t = threading.Thread(target=ping , args=(f"192.168.0.{x}",))
+        threads.append(t)
+    try:
+        for x in range(a):
+            threads[x].start()
+
+        for x in range(a):
+            threads[x].join()
+
+    except Exception as e :
+        print(e)
+        pass
+
+if __name__ == "__main__":
+    main()
